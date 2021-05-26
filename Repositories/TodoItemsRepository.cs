375807b1ee4207b556cea43bfd6bbea28b1b2a37
John Guilding <jwguilding@gmail.com>
2021-05-26 20:18:04 +0100

    add toto items repository

---
+++ Repositories/TodoItemsRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Interfaces;
+using API.Models;
+
+namespace API.Repositories
+{
+    public class TodoItemsRepository : ITodoItemsRepository
+    {
+        public Task<IEnumerable<TodoItemDTO>> GetTodoItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TodoItemDTO> GetTodoItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TodoItemDTO> CreateTodoItem(TodoItemDTO todoItemDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
